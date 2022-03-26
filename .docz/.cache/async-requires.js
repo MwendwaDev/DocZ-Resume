8d7a4c139b8cbf2c14857d44a6db5b81047015fc
emmatech2 <emmahtech2@gmail.com>
2022-03-26 22:15:22 +0300

    Generate static files

--- .docz/.cache/async-requires.js
+++ .docz/.cache/async-requires.js
@@ -2,7 +2,6 @@
 const preferDefault = m => (m && m.default) || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---my-resume-readme-md": () => import("./../../../../my-resume/README.md" /* webpackChunkName: "component---my-resume-readme-md" */),
   "component---my-resume-src-index-mdx": () => import("./../../../../my-resume/src/index.mdx" /* webpackChunkName: "component---my-resume-src-index-mdx" */),
   "component---my-resume-src-wiki-mdx": () => import("./../../../../my-resume/src/wiki.mdx" /* webpackChunkName: "component---my-resume-src-wiki-mdx" */),
--- .docz/.cache/sync-requires.js
+++ .docz/.cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\user\\Desktop\\newapp\\.docz\\.cache\\dev-404-page.js"))),
   "component---my-resume-readme-md": hot(preferDefault(require("C:\\Users\\user\\Desktop\\newapp\\my-resume\\README.md"))),
   "component---my-resume-src-index-mdx": hot(preferDefault(require("C:\\Users\\user\\Desktop\\newapp\\my-resume\\src\\index.mdx"))),
   "component---my-resume-src-wiki-mdx": hot(preferDefault(require("C:\\Users\\user\\Desktop\\newapp\\my-resume\\src\\wiki.mdx"))),
