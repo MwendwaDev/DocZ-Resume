09efcddd4f8ac7a1ece6df76a417417e0f3cf422
emmatech2 <emmahtech2@gmail.com>
2022-03-26 16:54:20 +0300

    First commit

---
+++ .docz/.cache/sync-requires.js
@@ -0,0 +1,14 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\user\\Desktop\\newapp\\.docz\\.cache\\dev-404-page.js"))),
+  "component---my-resume-readme-md": hot(preferDefault(require("C:\\Users\\user\\Desktop\\newapp\\my-resume\\README.md"))),
+  "component---my-resume-src-index-mdx": hot(preferDefault(require("C:\\Users\\user\\Desktop\\newapp\\my-resume\\src\\index.mdx"))),
+  "component---my-resume-src-wiki-mdx": hot(preferDefault(require("C:\\Users\\user\\Desktop\\newapp\\my-resume\\src\\wiki.mdx"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\user\\Desktop\\newapp\\.docz\\src\\pages\\404.js")))
+}
+
